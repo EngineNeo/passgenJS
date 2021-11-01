dae3df4e072463af7d26c2bb4c54f6dc545b18e5
Neo Maralit <neomaralit@gmail.com>
2021-11-01 01:35:57 -0400

    Fixed range slider default value

--- index.js
+++ index.js
@@ -1,24 +1,42 @@
 generatedPassword = document.getElementById('generated-password')
 passLen = document.getElementById('pass-len')
+displayLen = document.getElementById('display-len')
+render()
 
 function generatePass() {
     let str = 'ABCDEGFHIGKLMNOPQRSTUVWXYZ'
     let specialChar = '!@#$%^&*?=-'
     let char = ''
+    let pass = ''
 
     function randomInt(max) {
-        return Math.floot(Math.random() * max)
+        return Math.floor(Math.random() * max)
     }
 
-    for(i=0; i < passLen; i++) {
+    for(i = 0; i < passLen; i++) {
         strorSpecial = randomInt(2)
-        if (strorSpecial === 1){
+        if (strorSpecial == 1){
             char = randomInt(str.length) + 1
-            return char
+            pass += str.charAt(char)
         }
-        else if (strorSpecial === 0) {
+        else if (strorSpecial == 0) {
             char = randomInt(specialChar.length) + 1
-            return char
+            pass += specialChar.charAt(char)
         }
     }
+    return pass
 }
+
+generateButton = document.getElementById('generate')
+function displayPass() {
+    generateButton.onclick = () => {
+        // generatedPassword.textContent = generatePass()
+        console.log(generatePass())
+    }
+}
+displayPass()
+
+function render() {
+    displayLen.textContent = passLen
+}
+render()
