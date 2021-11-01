7aa830561a95ce58e50c859fb195f59a0f88ea3b
Neo Maralit <neomaralit@gmail.com>
2021-10-31 22:05:18 -0400

    Logic for special char

--- index.js
+++ index.js
@@ -4,9 +4,21 @@ passLen = document.getElementById('pass-len')
 function generatePass() {
     let str = 'ABCDEGFHIGKLMNOPQRSTUVWXYZ'
     let specialChar = '!@#$%^&*?=-'
+    let char = ''
+
+    function randomInt(max) {
+        return Math.floot(Math.random() * max)
+    }
 
     for(i=0; i < passLen; i++) {
-        let char = Math.floor(Math.random()
-                              * str.length + 1)
+        strorSpecial = randomInt(2)
+        if (strorSpecial === 1){
+            char = randomInt(str.length) + 1
+            return char
+        }
+        else if (strorSpecial === 0) {
+            char = randomInt(specialChar.length) + 1
+            return char
+        }
     }
 }
